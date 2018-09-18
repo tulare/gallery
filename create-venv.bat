de82ae6fdf588d5f94956c52a158ca282108eb1d
tulare <tulare.paxgalactica@gmail.com>
2018-09-18 14:47:55 +0200

    Use pipenv for project dependencies.

--- create-venv.bat
+++
@@ -1,7 +0,0 @@
-@SETLOCAL
-@echo %0
-@py.exe -m venv --clear venv
-@venv\Scripts\python -m pip install --upgrade pip setuptools wheel
-@venv\Scripts\python -m pip install --find-links=wheels --upgrade -r run.req
-@venv\Scripts\python -m pip list
-@ENDLOCAL
--- run.bat
+++ run.bat
@@ -1,9 +1,4 @@
 @SETLOCAL
-@CALL venv\Scripts\activate
-@IF %ERRORLEVEL% EQU 0 GOTO :run
-@CALL create-venv.bat
-@CALL venv\Scripts\activate
-:run
-@python --version
-@python run.py %*
+@CALL py -m pipenv sync
+@CALL py -m pipenv run python run.py %*
 @ENDLOCAL
