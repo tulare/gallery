4e9fbaef74cfb9094dbc4a5c6bc8c62f5364cfa2
tulare <tulare.paxgalactica@gmail.com>
2018-09-27 17:19:31 +0200

    add IDLE for pipenv

--- idle.bat
+++ idle.bat
@@ -3,5 +3,5 @@
 @PATH=C:\Program Files\Git\cmd;%PATH%
 @SET PIPENV_PIPFILE=%APP_DIR%\Pipfile
 @SET PIPENV_VENV_IN_PROJECT=1
-@py -m pipenv run python %APP_DIR%\run.py %*
+@START "IDLE" /B /D %APP_DIR% py -m pipenv run pythonw -m idlelib %*
 @ENDLOCAL
