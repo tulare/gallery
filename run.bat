e299e0c03412d5927d5e4481bf69fd22954e215c
tulare <tulare.paxgalactica@gmail.com>
2018-11-21 10:09:05 +0100

    Change launching method

--- run.bat
+++ run.bat
@@ -1,7 +1,18 @@
 @SETLOCAL
 @SET APP_DIR=%~dp0
-@PATH=C:\Program Files\Git\cmd;%PATH%
 @SET PIPENV_PIPFILE=%APP_DIR%\Pipfile
 @SET PIPENV_VENV_IN_PROJECT=1
-@py -m pipenv run python %APP_DIR%\run.py %*
+
+@REM --- check venv ---
+@FOR /F %%V in ('py -m pipenv --venv') DO @SET _venv=%%V
+@IF NOT EXIST "%_venv%" GOTO :novenv
+
+@REM --- run python in venv ---
+@FOR /F %%P in ('py -m pipenv --py') DO @%%P %APP_DIR%\main.py %*
+@GOTO end
+
+:novenv
+@ECHO Python virtual environement not found
+
+:end
 @ENDLOCAL
